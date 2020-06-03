d19bf04f28bdfcad650f3ff9e85b6df4712dff91
felipearb <39956315+felipearb@users.noreply.github.com>
2020-06-03 20:44:20 -0300

    Update Astar.py

--- Astar/Astar.py
+++ Astar/Astar.py
@@ -1,9 +1,29 @@
-from labirinto import main, listLabirinto, plotLabirinto, heuristica
-from queue import PriorityQueue
+from labirinto import main, listLabirinto, plotLabirinto
 
-def getNeighbors(rows, columns, estAtual):
-    row = estAtual[0]
-    column = estAtual[1]
+class posicao():
+    def __init__(self,a,b,c):
+        self.prioridade=a
+        self.linha=b
+        self.coluna=c
+    def get_prioridade(self):
+        return self.prioridade
+    
+class priorityQueue():
+    def __init__(self):
+        self.PQ = list()
+        self.ordenado =True
+    def add(self,posicao):
+        self.PQ.append(posicao)
+        self.ordenado =False
+    def pop(self):
+        if self.ordenado == False:
+            self.PQ.sort(self.PQ, key = posicao.get_prioridade(self),reverse=True)
+            ordenado=True
+        ret= PQ[0]
+        self.PQ[0].remove()
+        return ret
+
+def getNeighbors(rows, columns, row,column):
     neighbors = []
 
     if row == 0:
@@ -53,29 +73,35 @@ def getNeighbors(rows, columns, estAtual):
 
     return neighbors
 
+def heuristica(estFinal, estAtual, estPossivel):
+        atual = abs(estFinal[0] - estAtual[0]) + abs(estFinal[1] - estAtual[1])
+        possivel = abs(estFinal[0] - estPossivel[0]) + abs(estFinal[1] - estPossivel[1])
+        return atual + possivel
 
-def Astar(lab, estAtual, estFinal):
-    explored = []
-    PQ =PriorityQueue()
-    PQ.put(heuristica(estAtual,estAtual),estAtual)
 
+def Astar(lab, estInicial, estFinal):
+    caminho=[]
+    PQ = priorityQueue()
+    PQ.add((heuristica(estFinal,estInicial,estInicial),estAtual))
+    
     while PQ:
-        path = PQ.pop()
-        node = path[-1]
-
-        if node not in explored:
-            neighbors = getNeighbors(len(lab.data), len(lab.data[0])-1, node)
-
-            for n in neighbors:
-                if lab.data[n[0]][n[1]] > 0:
-                    new_path = list(path)
-                    new_path.append(n)
-                    PQ.put(heuristica(estAtual,lab.data),estAtual)
-
-                    if n == estFinal:
-                        return new_path
-
-            explored.append(node)
+        node = PQ.pop()
+        neighbors = getNeighbors(len(lab.data), len(lab.data[0])-1, node[0],node[1])
+        
+        for n in neighbors:
+            if lab.data[n[0]][n[1]] > 0:
+                   PQ.add(heuristica(estFinal,estAtual,lab.data),estAtual)
+                   
+                   if n == estFinal:
+                       caninho.add(estFinal)
+                       node = node.pai()
+                       
+                       while node != estInicial:
+                           caminho.add(node)
+                           node = node.pai()
+                       
+                       caminho.add(estInicial)
+        #node.pri=9999999
 
 if __name__ == '__main__':
     lab = listLabirinto("labirinto.txt")
@@ -88,6 +114,6 @@ if __name__ == '__main__':
     print(caminho)
 
     for line in lab.data:
-      print(line)
+        print(line)
 
     plotLabirinto(lab.data, caminho, lab.linha, lab.coluna)
